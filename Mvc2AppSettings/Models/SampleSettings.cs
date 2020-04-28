977b7a54584d9cf31c23c3139f0e11c04e32027a
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-04-28 10:55:42 +0200

    Add project files.

---
+++ Mvc2AppSettings/Models/SampleSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Mvc2AppSettings.Models
+{
+    public class SampleSettings
+    {
+        public string HeaderFirstPage { get; set; }
+        public int RepeatTimes { get; set; }
+    }
+}
